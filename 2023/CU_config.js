c596ab9020d3d72b57aae96e7991b2b7afcba838
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-01-18 09:20:07 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022flwp",
+      "defaultValue": "2022carv",
       "required": "true"
     },
     { "name": "Match Level",
