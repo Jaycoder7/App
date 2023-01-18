d99b3977e8228e9eaab625c96f93a75b502d197f
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-01-18 12:58:21 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "sdoMfw6ztjwn1qlkf7ASUIErdXZx5IP9AeFlmmeLWTeUmgFosHCjsvrbCKkJmgRf";
 /**
  * Get list of teams in event
  *
