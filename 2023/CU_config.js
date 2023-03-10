9fbb20039f4abf6bf498eb20f032a5bcf0439a63
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-03-09 23:07:23 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023okok",
+      "defaultValue": "2023gadal",
       "required": "true",
       "disabled": "true"
     },
