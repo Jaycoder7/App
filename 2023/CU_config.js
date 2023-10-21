d17a577729d676212b955609ef64e9c54e200b70
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-10-21 08:43:13 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023gacmp",
+      "defaultValue": "",
       "required": "true",
       "disabled": "true"
     },
